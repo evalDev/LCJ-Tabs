641a57250d8b00b09a85c37c2899f64002a05939
@evalDev <WhatAGit@developingdan.com>
2016-07-18 00:22:08 +0100

    added node version checking

--- Jakefile.js
+++ Jakefile.js
@@ -2,8 +2,17 @@
   "use strict";
 
   desc("Default Task");
-  task("default", function(){
-    console.log("Hello, I'm the default task");
+  task("default",[ "version"], function(){
     console.log("\n\nBuild OK!!");
+  });
+  desc("Check Node version");
+  task("version", function(){
+    var packageJson = require('./package.json');
+    var expectedVersion ="v"+ packageJson.engines.node;
+    var actualVersion = process.version;
+    if (actualVersion !== expectedVersion){
+      fail( "Incorect Node version: expected " + expectedVersion +", but was " + actualVersion);
+    }
+    console.log("Checking Node version: .");
   });
 }());
