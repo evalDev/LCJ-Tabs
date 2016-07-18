4f1969eb93b1f1e3b6de9eec308ae9da8701eae0
@evalDev <WhatAGit@developingdan.com>
2016-07-18 10:29:24 +0100

    can now use jake to run a local server

--- Jakefile.js
+++ Jakefile.js
@@ -1,11 +1,21 @@
 /* globals desc: false, jake:false, task:false, complete: false, fail: false */
 (function(){
   "use strict";
+  
+  //General Purpose Tasks
 
   desc("Default Task");
   task("default",[ "version", "lint"], function(){
     console.log("\n\nBuild OK!!");
   });
+
+  desc("Run a Localhost Server");
+  task("run", function(){
+    console.log("Run http-server here");
+    jake.exec("node node_modules/http-server/bin/http-server src", {interactive: true}, complete);
+  });
+  // Supporting Tasks
+  
   desc("Check Node version");
   task("version", function(){
     var packageJson = require('./package.json');
